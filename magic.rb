b82a2c7f8ed6c83555ec4ed992e305a84dbbdbbe
Raj Shah <brainix@gmail.com>
2012-06-23 16:57:19 -0700

    tried to get all user Tweets, blew out our Twitter API rate limit  :-(

--- magic.rb
+++ magic.rb
@@ -2,4 +2,19 @@ require 'twitter'
 
 
 module Magic
+  def self.get_timeline(username)
+    timeline = Twitter.user_timeline(username, count: 200)
+
+    loop do
+      tweet_count = timeline.count
+      begin
+        timeline += Twitter.user_timeline(username, count: 200, max_id: timeline.last.id)
+      rescue Twitter::Error::BadGateway
+      end
+      puts timeline.count
+      break if tweet_count == timeline.count
+    end
+
+    timeline
+  end
 end
