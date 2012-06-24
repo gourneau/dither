d26e90b324d9b77113d5a0b9596143e4b115a3e5
Ben Woosley <ben.woosley@gmail.com>
2012-06-23 19:52:12 -0700

    Use the singleton class in Magic

--- magic.rb
+++ magic.rb
@@ -9,21 +9,23 @@ module Magic
   CONSUMER_KEY = '0W95ajy4zYj4C7qcjn1Tg'
   CONSUMER_SECRET = 'bCFVFAWx0WS5NmPBhuxR5u3tiEnftYVXHpKHVB7xZbo'
 
-  def self.to_twitter
-    oauth_consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: 'http://api.twitter.com', request_endpoint: 'http://api.twitter.com', sign_in: true)
-    request_token = oauth_consumer.get_request_token(oauth_callback: 'http://olivebranch.herokuapp.com/')
-    request_token
-  end
+  class << self
+    def to_twitter
+      oauth_consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: 'http://api.twitter.com', request_endpoint: 'http://api.twitter.com', sign_in: true)
+      request_token = oauth_consumer.get_request_token(oauth_callback: 'http://olivebranch.herokuapp.com/')
+      request_token
+    end
 
-  def self.from_twitter(request_token, oauth_token, oauth_verifier)
-    oauth_consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: 'http://api.twitter.com', request_endpoint: 'http://api.twitter.com', sign_in: true)
-    access_token = request_token.get_access_token(oauth_verifier: oauth_verifier)
-    timeline = access_token.request(:get, "http://api.twitter.com/1/statuses/user_timeline.json?count=200")
-    timeline
-  end
+    def from_twitter(request_token, oauth_token, oauth_verifier)
+      oauth_consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: 'http://api.twitter.com', request_endpoint: 'http://api.twitter.com', sign_in: true)
+      access_token = request_token.get_access_token(oauth_verifier: oauth_verifier)
+      timeline = access_token.request(:get, "http://api.twitter.com/1/statuses/user_timeline.json?count=200")
+      timeline
+    end
 
-  def self.magic(timeline)
-    timeline
+    def magic(timeline)
+      timeline
+    end
   end
 end
 
