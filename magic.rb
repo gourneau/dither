8afc98f4b1656b4eb65fb4d1730edd0e120efc2f
Raj Shah <brainix@gmail.com>
2012-06-23 17:07:41 -0700

    authenticated app with Twitter

--- magic.rb
+++ magic.rb
@@ -1,7 +1,32 @@
+#!/usr/bin/env ruby
+
+
+require 'oauth'
 require 'twitter'
 
 
 module Magic
+  CONSUMER_KEY = '0W95ajy4zYj4C7qcjn1Tg'
+  CONSUMER_SECRET = 'bCFVFAWx0WS5NmPBhuxR5u3tiEnftYVXHpKHVB7xZbo'
+  OAUTH_TOKEN = 'H7jzvkMyFefAMlPidOXayR0oyp4SkQm7lgio1ZGAbI'
+  OAUTH_SECRET = 'JdBmFe4PPtgXlUZH9p7Nai4fapOIya9OfVwi43Q'
+
+  def self.consumer_to_oauth
+    oauth_consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: 'http://api.twitter.com', request_endpoint: 'http://api.twitter.com', sign_in: true)
+    request_token = oauth_consumer.get_request_token
+    puts 'OAuth Token: ' + request_token.token
+    puts 'OAuth Secret: ' + request_token.secret
+  end
+
+  def self.authenticate
+    Twitter.configure do |config|
+      config.consumer_key = CONSUMER_KEY
+      config.consumer_secret = CONSUMER_SECRET
+      config.oauth_token = OAUTH_TOKEN
+      config.oauth_token_secret = OAUTH_SECRET
+    end
+  end
+
   def self.get_timeline(username)
     timeline = Twitter.user_timeline(username, count: 200)
 
@@ -17,4 +42,9 @@ module Magic
 
     timeline
   end
+end
+
+
+if __FILE__ == $0
+  Magic.consumer_to_oauth
 end
