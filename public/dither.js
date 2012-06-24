426f939a7b8a57efa1beb3bedf595638761a9016
Raj Shah <brainix@gmail.com>
2012-06-23 21:00:39 -0700

    snap back to reality

--- public/dither.js
+++ public/dither.js
@@ -8,24 +8,8 @@ Dither = {
       return false;
     }
 
-    twttr.anywhere(function (T) {
-      T("#tbox").tweetBox({
-        height: 100,
-        width: 400,
-        defaultContent: "<YOUR DEFAULT TWEETBOX CONTENT HERE>"
-      });
-    });
-
     // We're now initialized.
     return this._initialized = true;
-  },
-
-  _username: function() {
-    var username = $("[name='username']").val();
-    $.getJSON('/magic', {username: username}, function(data) {
-      console.log(data);
-    });
-    return false;
   }
 };
 
