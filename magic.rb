55b275633d5ab0a6d48afa57a6e1e5144b940123
Ben Woosley <ben.woosley@gmail.com>
2012-06-23 19:54:44 -0700

    Extract Magic.oauth_consumer

--- magic.rb
+++ magic.rb
@@ -10,16 +10,24 @@ module Magic
   CONSUMER_SECRET = 'bCFVFAWx0WS5NmPBhuxR5u3tiEnftYVXHpKHVB7xZbo'
 
   class << self
+    def oauth_consumer
+      OAuth::Consumer.new(
+        CONSUMER_KEY, CONSUMER_SECRET,
+        site: 'http://api.twitter.com',
+        request_endpoint: 'http://api.twitter.com',
+        sign_in: true)
+    end
+
     def to_twitter
-      oauth_consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: 'http://api.twitter.com', request_endpoint: 'http://api.twitter.com', sign_in: true)
-      request_token = oauth_consumer.get_request_token(oauth_callback: 'http://olivebranch.herokuapp.com/')
+      request_token = oauth_consumer.get_request_token(
+        oauth_callback: 'http://olivebranch.herokuapp.com/')
       request_token
     end
 
     def from_twitter(request_token, oauth_token, oauth_verifier)
-      oauth_consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site: 'http://api.twitter.com', request_endpoint: 'http://api.twitter.com', sign_in: true)
       access_token = request_token.get_access_token(oauth_verifier: oauth_verifier)
-      timeline = access_token.request(:get, "http://api.twitter.com/1/statuses/user_timeline.json?count=200")
+      timeline = access_token.request(:get,
+        "http://api.twitter.com/1/statuses/user_timeline.json?count=200")
       timeline
     end
 
